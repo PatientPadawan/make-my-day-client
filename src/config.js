b8a7f9786782d28b3727f297272705292e84e716
Alex Marshall Braden <1patientpadawan@gmail.com>
2020-01-31 13:35:48 -0700

    refactored to use env variable instead of hard coded redirect uri

--- src/config.js
+++ src/config.js
@@ -2,6 +2,5 @@ export default {
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
   issuer: `https://${process.env.REACT_APP_OKTA_DOMAIN}/oauth2/default`,
   clientId: `${process.env.REACT_APP_OKTA_ID}`,
-  redirectUri: 'https://make-my-day-events.now.sh/implicit/callback',
-  // redirectUri: `${window.location.origin}/implicit/callback`,
+  redirectUri: `${process.env.REACT_APP_CLIENT_BASE_URL}/implicit/callback`,
 };
