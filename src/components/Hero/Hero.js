21178cb21871527b1adca14b0f4ea3f7094447c8
Alex Marshall Braden <1patientpadawan@gmail.com>
2020-01-31 16:15:58 -0700

    added empty alt tags for decorative photos

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,43 +6,63 @@ export default function Hero() {
   const images = [
     {
       original: 'https://i.imgur.com/jabsphK.jpg',
+      originalAlt: '',
       thumbnail: 'https://i.imgur.com/jabsphKt.jpg',
+      thumbnailAlt: '',
     },
     {
       original: 'https://i.imgur.com/jaBkAal.jpg',
+      originalAlt: '',
       thumbnail: 'https://i.imgur.com/jaBkAalt.jpg',
+      thumbnailAlt: '',
     },
     {
       original: 'https://i.imgur.com/D6cIbb5.jpg',
+      originalAlt: '',
       thumbnail: 'https://i.imgur.com/D6cIbb5t.jpg',
+      thumbnailAlt: '',
     },
     {
       original: 'https://i.imgur.com/TJmPvG2.jpg',
+      originalAlt: '',
       thumbnail: 'https://i.imgur.com/TJmPvG2t.jpg',
+      thumbnailAlt: '',
     },
     {
       original: 'https://i.imgur.com/PkrhU9D.jpg',
+      originalAlt: '',
       thumbnail: 'https://i.imgur.com/PkrhU9Dt.jpg',
+      thumbnailAlt: '',
     },
     {
       original: 'https://i.imgur.com/wm9V76i.jpg',
+      originalAlt: '',
       thumbnail: 'https://i.imgur.com/wm9V76it.jpg',
+      thumbnailAlt: '',
     },
     {
       original: 'https://i.imgur.com/f92GeES.jpg',
+      originalAlt: '',
       thumbnail: 'https://i.imgur.com/f92GeESt.jpg',
+      thumbnailAlt: '',
     },
     {
       original: 'https://i.imgur.com/VZPEHgu.jpg',
+      originalAlt: '',
       thumbnail: 'https://i.imgur.com/VZPEHgut.jpg',
+      thumbnailAlt: '',
     },
     {
       original: 'https://i.imgur.com/jh5lwiN.jpg',
+      originalAlt: '',
       thumbnail: 'https://i.imgur.com/jh5lwiNt.jpg',
+      thumbnailAlt: '',
     },
     {
       original: 'https://i.imgur.com/1CC7oX6.jpg',
+      originalAlt: '',
       thumbnail: 'https://i.imgur.com/1CC7oX6t.jpg',
+      thumbnailAlt: '',
     },
   ];
 
